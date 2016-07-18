b97d209fe294bfc01e11b4abce16a4be6df54572
Danil Nikiforov <danil.nikiphorov@gmail.com>
2016-07-18 19:28:14 +0600

    add backlog. fix some bugs. fix output format for reliable scrum backlog table

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var Converter = require("csvtojson").Converter;
+var json2csv = require('json2csv');
+
 var cardsConverter = new Converter({});
 var historyConverter = new Converter({});
 var _ = require('lodash');
 var moment = require('moment');
+
+
+// =====================================================================================
+// =============== Extract =============================================================
 
 //end_parsed will be emitted once parsing finished 
 cardsConverter.on("end_parsed", function (cardsArray) {
@@ -10,41 +16,110 @@ cardsConverter.on("end_parsed", function (cardsArray) {
 
 	historyConverter.on("end_parsed", function (historyArray) {
 		
+
+// =====================================================================================
+// =============== Transform ===========================================================
+
 		// посчитать время за которое мы делаем N задач. За неделю мы делаем столько то пойнтов
 		// 1. находим все карты, перешедшие в Ready for UAT за неделю
 		// 2. оставляем только уникальные
 		// 3. фильтруем по полям карточек
 
 		var startDate = new Date('2016-07-11');
-		var results = getStatsForTimePeriod(1,startDate, historyArray);
+		var results = getFinishedCardsForTimePeriod(1, startDate, historyArray, 'TT Quality Assurance: Ready for QA');
+		// var results = getFinishedCardsForTimePeriod(1, startDate, historyArray, 'UAT: Ready for UAT');
 		console.log(results);
-		var filteredResults  = customFilter(results, cardsArray);
+		var joinedResults = joinCardsWithDescription(results, cardsArray);
+		// console.log(joinedResults);
 
-		filteredResults = addCreatedDateForCards(filteredResults, historyArray);
+		var filteredFinishedCardsResults  = customFilter(joinedResults);
+
+		var backlog = getBackLog(cardsArray);
+
+		backlog = setFinishedDateToUnfinished(backlog);
+
+		//console.log('backlog: ', backlog);
+		var filteredResults = _.concat(filteredFinishedCardsResults, backlog);
+
+		filteredResults = addCreatedDateForCards(filteredResults, historyArray, startDate);
 		console.log("=================================");
 		//console.log(filteredResults);
-		_.each(filteredResults, function(item, key) {
-			console.log(item.Card + ", " + item['Finished at day']);
-		})
+
+// =====================================================================================
+// =============== Load ================================================================
+
+		var exportArray = _.map(filteredResults, function(item, key) {
+			return {
+				'Card Name': item.ExternalCardID + ' ' + item.Card_Title,
+				'Estimate': item.Card_Size,
+				'Date added': item['Date added'],
+				'Finished at day': item['Finished at day']
+			};
+		});
+		//console.log(exportArray);
+		var fs = require('fs');
+		var fields = ['Card Name', 'Estimate', 'Date added', 'Finished at day'];
 		
+		var csv = json2csv({ data: exportArray, fields: fields });
+
+		fs.writeFile('r-scrum.csv', csv, function(err) {
+		  if (err) throw err;
+		  console.log('file saved');
+		});
 	});
 
 });
 
 
-function customFilter(results, cardsArray) {
+
+// =====================================================================================
+// =====================================================================================
+
+function setFinishedDateToUnfinished(backlog) {
+	return  _.map(backlog, function(element) { 
+		return _.extend({}, element, {'Finished at day': 'unfinished'});
+	});
+}
+
+function getBackLog(cardsArray) {
+ 	var filteredResults = _.filter(cardsArray, function(card) {
+		return cardMatchesBacklogFilter(card);
+	});
+	
+ 	return filteredResults;
+} 
+
+
+ function cardMatchesBacklogFilter(cardDesc) { //TODO: merge with cardMatchesFilter
+ 	// console.log('cardDesc:', cardDesc);
+ 	var matched = false;
+ 	if(cardDesc.Card_Type === 'UAT Feedback' && cardDesc.Card_Priority === 'Critical' && isCardUdone(cardDesc.Lane_Title)){
+ 		matched = true;
+ 	}
+
+ 	return matched;
+ }
+
+ function isCardUdone(lineTitle) { //TODO: merge with cardMatchesFilter
+ 	if(lineTitle === 'backlog:uat feedback' || lineTitle === 'sprint log' || lineTitle === 'development:in progress' || lineTitle === 'development:review'){
+ 		console.log('???lineTitle: ', lineTitle)
+ 		return true;
+ 	}
+ 	return false;
+ }
+
+function customFilter(results) {
  	// console.log(cardsArray[0]);
  	// console.log(results[0]);
  	
 	var filteredResults = _.filter(results, function(card) {
-		var cardDesc = cardDescriptionById(card['Card Id'], cardsArray);
-		return cardMatchesFilter(cardDesc);
+		return cardMatchesFilter(card);
 	});
 	
  	return filteredResults;
  }
 
- function cardMatchesFilter(cardDesc) {
+ function cardMatchesFilter(cardDesc) { //TODO: merge with isCardUdone
  	var matched = false;
  	if(cardDesc.Card_Type === 'UAT Feedback' && cardDesc.Card_Priority === 'Critical'){
  		matched = true;
@@ -58,13 +133,12 @@ function customFilter(results, cardsArray) {
  	return result;
  }
 
-function getStatsForTimePeriod(numOfDays,startDate, historyArray) {
+function getFinishedCardsForTimePeriod(numOfDays,startDate, historyArray, finishedLine) {
 	var result = [];
-	var finishedLine = 'UAT: Ready for UAT';
 	var currentDate = startDate;
 	var i = 0;
 	var now = new Date();
-	// while(currentDate<=now){
+	while(currentDate<=now){
 		
 		var endDate = new Date(currentDate);
 		endDate.setDate(endDate.getDate() + numOfDays);
@@ -84,7 +158,7 @@ function getStatsForTimePeriod(numOfDays,startDate, historyArray) {
   		
 		currentDate = new Date(endDate);
 		i++;
-	// }
+	}
 	return result;
 }
 
@@ -95,11 +169,11 @@ function getStatsForTimePeriod(numOfDays,startDate, historyArray) {
  		var toLane = 'To Lane';
 		var whenDate = new Date(moment(currentDate, 'MM/DD/YYYY [at] hh:mm:ss A').format());
 		var correctDate = false;
-		// console.log(o);
+		// console.log(o[toLane]);
  		if(whenDate>=startDate && whenDate <=endDate)
  			correctDate = true; 
  		if(correctDate && o[toLane] === columnName){
- 			// console.log(o['Card Id']);
+ 			console.log(o['Card Id']);
  			return true;
  		}else{
  			return false;
@@ -107,10 +181,48 @@ function getStatsForTimePeriod(numOfDays,startDate, historyArray) {
  	});
  }
 
- function addCreatedDateForCards(cards, historyArray) {
- 	return cards
+ function addCreatedDateForCards(cards, historyArray, startDate) {
+ 	return _.map(cards, function(card) {
+ 		var createdDate = findCreatedDateForCardId(card['Card Id'], historyArray);
+ 		var momentDate =  moment(createdDate, 'MM/DD/YYYY [at] hh:mm:ss A');
+ 		if(momentDate.isValid()){
+ 			var createdDateObject = new Date(momentDate.format());
+ 			console.log('???????? createdDateObject:', createdDateObject);
+ 			var timeDiff = createdDateObject.getTime() - startDate.getTime();
+			var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+			if(diffDays<0){
+				diffDays = 0;
+			}
+			console.log(diffDays);
+ 			card['Date added'] = diffDays;
+ 		}else{
+ 			card['Date added'] = 'unknown date';
+ 		}
+ 		
+ 		return card;
+ 	});
  }
 
+function findCreatedDateForCardId(cardId, historyArray) {
+	// console.log('cardId: ', cardId);
+	var record = _.find(historyArray, {'What': 'Card Creation Event', 'Card Id': cardId});
+	// console.log(record);
+	var createdDate;
+	if(!_.isNil(record)){
+		createdDate = record.When;
+	}
+	return createdDate;
+}
+
+
+function joinCardsWithDescription(results, cardsDesc) {
+
+	return _.map(results, function (cardHistory) {
+		var fullCardDescr = cardDescriptionById(cardHistory['Card Id'], cardsDesc);
+		return _.extend(cardHistory, fullCardDescr);
+	});
+}
+
 //read from file 
-require("fs").createReadStream("./cards (6).csv").pipe(cardsConverter);
-require("fs").createReadStream("./eventsexport.csv").pipe(historyConverter);
+require("fs").createReadStream("./cards (7).csv").pipe(cardsConverter);
+require("fs").createReadStream("./eventsexport (2).csv").pipe(historyConverter);
